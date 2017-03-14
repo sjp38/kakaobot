ac16b1bc0871aa9310f177aeac1137d44b0ecd91
SeongJae Park <sj38.park@gmail.com>
2017-03-14 19:23:08 +0900

    Implement basic http server
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- kakaobot.go
+++ kakaobot.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+func handleHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "received %s %s",
+		r.Method, html.EscapeString(r.URL.Path))
+}
 
 func main() {
-	fmt.Printf("hello\n")
+	http.HandleFunc("/kakaobot/", handleHTTP)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Printf("buy\n")
 }
